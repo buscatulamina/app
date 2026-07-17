b2230f46c51665fc1a2b9302353a496734b09e7e
buscatulamina <buscatulamina@gmail.com>
2026-07-16 20:22:17 -0400

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,10 +2,14 @@ from .property import Property, PropertyCreate, PropertyUpdate
 from .testimonial import Testimonial, TestimonialCreate
 from .contact import Contact, ContactCreate
 from .property_inquiry import PropertyInquiry, PropertyInquiryCreate
+from .visit import Visit, VisitCreate
+from .whatsapp_contact import WhatsappContact, WhatsappContactCreate
 
 __all__ = [
     'Property', 'PropertyCreate', 'PropertyUpdate',
     'Testimonial', 'TestimonialCreate',
     'Contact', 'ContactCreate',
-    'PropertyInquiry', 'PropertyInquiryCreate'
+    'PropertyInquiry', 'PropertyInquiryCreate',
+    'Visit', 'VisitCreate',
+    'WhatsappContact', 'WhatsappContactCreate'
 ]
